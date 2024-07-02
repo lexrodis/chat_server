1f49d337621b7cdb3388e0c32df485a88f1cd7f8
AlexSidorov12 <115104583+AlexSidorov12@users.noreply.github.com>
2024-07-02 18:24:32 +0000

    -

--- user_input.py
+++ user_input.py
@@ -3,11 +3,7 @@
 def user_numbers(numbers_string):
     numbers_string
     numbers = numbers_string.split()
-    result = []
-    for n in numbers:
-        result.append(int(n))
-
-    return result
+    return [int(n) for n in numbers]
 
 
 import unittest
