bebeccb9e5ef3198acd6f04d9ea8f70fd0cec4b4
dezerdigah_duo <86304474+dezerdigah-duo@users.noreply.github.com>
2024-07-03 09:01:50 +0000

    --

--- user_input.py
+++ user_input.py
@@ -1,7 +1,10 @@
 def user_numbers(numbers_string):
     numbers_string
     numbers = numbers_string.split()
-    return [int(n) for n in numbers]
+    try:
+        return [int(n) for n in numbers]
+    except ValueError as e:
+        raise ValueError(f"Invalid input: {e}")
 
 
 import unittest
@@ -11,10 +14,12 @@ class TestUserInput(unittest.TestCase):
     def test_user_numbers(self):
         self.assertEqual(user_numbers("1 2 3"), [1,2,3])
 
-        def test_user_input_error(self):
-           self.assertRaises(ValueError, test_user_input_error("oiu oiu"))
-
-
+    def test_user_numbers_error(self):
+        with self.assertRaises(ValueError):
+            user_numbers("oiu oui")
+    
+    def test_user_numbers_error_no_with(self):
+        self.assertRaises(ValueError, user_numbers, "oiu oui")
     
 if __name__ == '__main__':
     unittest.main()
