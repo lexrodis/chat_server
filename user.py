b74d1119c2a7fb2835b3909d94b1f4757ef7f371
AlexSidorov12 <115104583+AlexSidorov12@users.noreply.github.com>
2024-07-09 17:00:18 +0000

    --

---
+++ user.py
@@ -0,0 +1,30 @@
+import uuid
+
+
+class ChatUser:
+    def __init__(self, user_name = "anonymous") -> None:
+        self.id = uuid.uuid4()
+        self.user_name = user_name
+        
+    def get_uuid(self):
+        return str(self.id)
+    
+
+    def send_message(self, message):
+        print(f'{message}\n')
+        
+    
+
+
+import unittest
+
+class TestUserInput(unittest.TestCase):
+    def test_get_uuid(self):
+        user = ChatUser("Ronaldo")
+        self.assertTrue(isinstance(user.get_uuid(), str))
+
+    def test_default_user_name(self):
+        user = ChatUser()
+        self.assertEquals(user.user_name, "anonymous")
+
+    def test
